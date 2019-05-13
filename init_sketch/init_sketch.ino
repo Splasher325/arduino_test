8074351378a79677e35c4be0c6b73d3fb4301b6e
Joseph Brown <jbrown3859@gmail.com>
2019-05-13 11:11:27 -0700

    actually fixed frequency, comments

--- init_sketch/init_sketch.ino
+++ init_sketch/init_sketch.ino
@@ -1,39 +1,25 @@
 #include <Servo.h>
-
 Servo ESC;
 int potpin=6;
 int input;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   ESC.attach(6);
-
-  
-  /*
-  Serial.begin(9600);
-  Serial.print(0);
-  axis_pin = A0;
-  sig_pin = 6;
-  pinMode(sig_pin, OUTPUT);
-  */
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Read analog input
   input = analogRead(potpin);
-  input=map(input, 0, 1023, 0, 179);
-  ESC.write(input);
-  Serial.println(digitalRead(12));
-  delay(50);
   
-  /*
-  input = analogRead(axis_pin);
-  Serial.print(input);
-  output = map(input, 0, 1023, 0, 255);
-  digitalWrite(sig_pin, output);
-  */
+  // Map for Servo signal
+  input=map(input, 0, 1023, 0, 179);
 
-
-  //delay(20);
+  // Send digital signal out D6
+  ESC.write(input);
+  
+  // Digital output on Serial Monitor
+  // Serial.println(digitalRead(12));
+  
+  // Delay 20ms for 50Hz PWM signal
+  delay(20);
 }
