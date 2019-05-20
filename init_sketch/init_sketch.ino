25ef0d3860651d5ef43ce6ca1f7a042b82d54ede
Joseph Brown <jbrown3859@gmail.com>
2019-05-19 19:21:19 -0700

    better code

--- init_sketch/init_sketch.ino
+++ init_sketch/init_sketch.ino
@@ -7,24 +7,25 @@ int swpin=3;
 void setup() {
   Serial.begin(9600);
   ESC.attach(6);
+  ESC.writeMicroseconds(1000);
 }
 
 void loop() {
   // Read analog input
   // Serial.println(analogRead(3)); // 0 if pushed
-  if(analogRead(swpin)==0) {
+  if(analogRead(swpin)==0) { //Safety button
     input = analogRead(potpin);
     // Serial.println("pushed");
     // Map for Servo signal
-    input=map(input, 0, 1023, 0, 179);
+    input=map(input, 0, 1023, 1000, 2000); //Synthesizing the pwm siganl with SERVO library
   
     // Send digital signal out D6
   
-    ESC.write(input);
+    ESC.writeMicroseconds(input); //Sending synthesized signal
   }
   // Digital output on Serial Monitor
   // Serial.println(digitalRead(12));
   
   // Delay 20ms for 50Hz PWM signal
-  delay(20);
+  // delay(20);
 }
